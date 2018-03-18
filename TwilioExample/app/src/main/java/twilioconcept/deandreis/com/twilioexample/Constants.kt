82a0ab1490c769d4e11fcdb5c80433ced8ef3c96
Ignacio Deandreis Denis <ignacio@astropay.com>
2018-03-18 12:46:22 -0300

    CheckPermissions

---
+++ TwilioExample/app/src/main/java/twilioconcept/deandreis/com/twilioexample/Constants.kt
@@ -0,0 +1,6 @@
+package twilioconcept.deandreis.com.twilioexample
+
+/**
+ * Created by ignaciodeandreisdenis on 3/18/18.
+ */
+const val MIC_PERMISSION_REQUEST_CODE = 103
--- TwilioExample/app/src/main/java/twilioconcept/deandreis/com/twilioexample/MainActivity.kt
+++ TwilioExample/app/src/main/java/twilioconcept/deandreis/com/twilioexample/MainActivity.kt
@@ -1,12 +1,36 @@
 package twilioconcept.deandreis.com.twilioexample
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.RECORD_AUDIO)
+                == PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.RECORD_AUDIO), MIC_PERMISSION_REQUEST_CODE)
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            MIC_PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                } else {
+                    Toast.makeText(this, R.string.mobile_permission_denied, Toast.LENGTH_SHORT).show()
+                }
+
+            }
+        }
     }
 }
